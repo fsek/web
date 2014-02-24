3b5813024f122017692a352108bb2345de15d69c
Linus Hammarlund <linus.hammarlund@gmail.com>
2014-02-24 14:55:53 +0100

    Added Bootstrap 3

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Fsek
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Add /vendor/assets/fonts to autoload
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
