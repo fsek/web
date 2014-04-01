35e4c4af339a8bc04d524d8eff4f105f0f1ccb0c
David Wessman <Davidwessman@live.se>
2014-04-02 01:56:04 +0200

    fix the_role

--- config/initializers/the_role.rb
+++ config/initializers/the_role.rb
@@ -5,4 +5,5 @@ TheRole.configure do |config|
   config.default_user_role     = :user
   config.access_denied_method  = :access_denied      # define it in ApplicationController
   config.login_required_method = :authenticate_user! # devise auth method
+  config.first_user_should_be_admin = true;
 end
