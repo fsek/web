4e7ecdee15c991873a236e662105373d2c3335e5
David Wessman <Davidwessman@live.se>
2014-04-02 00:00:32 +0200

    user test

--- db/migrate/20140218203859_devise_create_users.rb
+++ db/migrate/20140218203859_devise_create_users.rb
@@ -20,7 +20,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
-      t.integer :role_id  :null => false, :default => 0
+      t.integer  :role_id  ,:null => false, :default => 0
       
 
       ## Confirmable
