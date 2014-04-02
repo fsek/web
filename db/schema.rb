27e20ee8123b6e4117237513be87117306b98d3b
Dirac <spindelman@fsektionen.se>
2014-04-02 23:13:22 +0200

    Fix

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20140328222934) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "role_id"
+    t.integer  "role_id",                default: 0,  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
