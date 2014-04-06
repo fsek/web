5f5f6a6f6a95764fcbad4b860c6488ebb5ff0063
David Wessman <Davidwessman@live.se>
2014-04-06 20:34:37 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -23,23 +23,23 @@ ActiveRecord::Schema.define(version: 20140418200461) do
     t.datetime "end_date"
     t.boolean  "dayevent"
     t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "news", force: true do |t|
     t.string   "title"
     t.text     "content"
+    t.string   "author"
+    t.boolean  "front_page"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "author"
-    t.boolean  "front_page"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -64,12 +64,12 @@ ActiveRecord::Schema.define(version: 20140418200461) do
     t.string   "program"
     t.integer  "start_year"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "roles", force: true do |t|
