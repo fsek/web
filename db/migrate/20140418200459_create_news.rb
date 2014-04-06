6c293fb5fc2e85928cea56602a68b50a132d6db4
David Wessman <Davidwessman@live.se>
2014-04-06 18:11:42 +0200

    fix

--- db/migrate/20140418200459_create_news.rb
+++ db/migrate/20140418200459_create_news.rb
@@ -2,8 +2,7 @@ class CreateNews < ActiveRecord::Migration
   def self.up
     create_table :news do |t|
       t.string  :title
-      t.text    :content
-      t.attachment :image
+      t.text    :content      
       t.string  :author
       t.boolean :front_page
       t.timestamps
