88b30cab5cd653e8355599719e285d6907342c6a
Dirac <spindelman@fsektionen.se>
2014-04-10 15:20:56 +0200

    asodo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140418200461) do
+ActiveRecord::Schema.define(version: 20140418200462) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -58,6 +58,11 @@ ActiveRecord::Schema.define(version: 20140418200461) do
     t.datetime "updated_at"
   end
 
+  create_table "posts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "profiles", force: true do |t|
     t.string   "name"
     t.string   "program"
@@ -97,7 +102,7 @@ ActiveRecord::Schema.define(version: 20140418200461) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
