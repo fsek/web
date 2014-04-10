0a4a42c27cbd198bd15fe9f6cb1711ce11122d7d
David Wessman <Davidwessman@live.se>
2014-04-10 20:15:41 +0200

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,19 +18,19 @@ Fsek::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
+# 
+  # config.action_mailer.smtp_settings = {
+  # address: “smtp.gmail.com”,
+  # port: 587,
+  # domain: ENV["GMAIL_DOMAIN"],
+  # authentication: “plain”,
+  # enable_starttls_auto: true,
+  # user_name: ENV["GMAIL_USERNAME"],
+  # password: ENV["GMAIL_PASSWORD"]
+  # }
 
-    config.action_mailer.smtp_settings = {
-    address: “smtp.gmail.com”,
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: “plain”,
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-    }
-
-  # Print deprecation notices to the Rails logger.
+  # # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,20 @@ Fsek::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
   config.action_mailer.default_url_options = { :host => 'fsektionen.se' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+  }
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
