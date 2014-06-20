fc8ed14baa4823d8c0122ea263caf2bcf0748b71
David Wessman <Davidwessman@live.se>
2014-06-20 12:02:15 +0200

    Ny storlek på bilder

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
   belongs_to :album 
   
   has_attached_file :foto, 
-                    :styles => { original: "4000x4000>", large: "800x800>", small: "250x250>",thumb: "100x100>" },                     
+                    :styles => { original: "4000x4000>", large: "1500x1500>", small: "250x250>",thumb: "100x100>" },                     
                     :path => ":rails_root/public/system/images/:attachment/:album_id/:id/:style/:filename",
                     :url => "/system/images/:attachment/:album_id/:id/:style/:filename" 
 
