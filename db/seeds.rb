42eb64aa8b728d1e77024def7440fa62aebe5bfa
Linus Hammarlund <linus.hammarlund@gmail.com>
2014-07-02 22:54:10 +0200

    Working seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+List.create!(:name => 'Styrelse',              :category => 'kontakt', :string1 => 'styrelse@fsektionen.se')
+List.create!(:name => 'Ordförande',            :category => 'kontakt', :string1 => 'ordf@fsektionen.se')
+List.create!(:name => 'Cafémästeriet',         :category => 'kontakt', :string1 => 'cafe@fsektionen.se')
+List.create!(:name => 'Överfös',               :category => 'kontakt', :string1 => 'overfos@fsektionen.se')
+List.create!(:name => 'Kulturministeriet',     :category => 'kontakt', :string1 => 'kultur@fsektionen.se')
+List.create!(:name => 'Näringslivsutskottet',  :category => 'kontakt', :string1 => 'fnu@fsektionen.se')
+List.create!(:name => 'Prylmästeriet',         :category => 'kontakt', :string1 => 'prylm@fsektionen.se')
+List.create!(:name => 'Sanningsministeriet',   :category => 'kontakt', :string1 => 'sanning@fsektionen.se')
+List.create!(:name => 'Sexmästeriet',          :category => 'kontakt', :string1 => 'sex@fsektionen.se')
+List.create!(:name => 'Utbildningsminister',   :category => 'kontakt', :string1 => 'um@fsektionen.se')
+List.create!(:name => 'Vice ordförande',       :category => 'kontakt', :string1 => 'viceordf@fsektionen.se')
+List.create!(:name => 'Kassör',                :category => 'kontakt', :string1 => 'kass@fsektionen.se')
+List.create!(:name => 'Sekreterare',           :category => 'kontakt', :string1 => 'sekreterare@fsektionen.se')
+List.create!(:name => 'Spindelmän',            :category => 'kontakt', :string1 => 'spindelman@fsektionen.se')
+List.create!(:name => 'Likabehandlingsombud',  :category => 'kontakt', :string1 => 'libo@fsektionen.se')
+
