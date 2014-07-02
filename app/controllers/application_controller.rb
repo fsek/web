26121d81aec49bc59cb888d72e4055e65baaa0b4
Linus Hammarlund <linus.hammarlund@gmail.com>
2014-07-02 22:24:09 +0200

    Require f-membership on sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,84 +1,69 @@
 class ApplicationController < ActionController::Base
   include TheRole::Controller
-  
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :exception  
-  
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) { |u| 
-      u.permit(:password, :password_confirmation, :current_password) 
-    }
-  end
-  
   protect_from_forgery
-    def access_denied
+  
+  before_filter :configure_permitted_devise_parameters, if: :devise_controller?
+  before_filter :set_locale
+
+  
+  def access_denied
     flash[:error] = t('the_role.access_denied')
     redirect_to(:back)
-        #Catch exception and redirect to root
+    
     rescue ActionController::RedirectBackError
       redirect_to root_path
   end
   
-  before_filter :set_locale
-  
   def authenticate_news!
-  flash[:error] = t('the_role.access_denied')
+    flash[:error] = t('the_role.access_denied')
     redirect_to(:back) unless current_user.admin? || current_user.moderator?(:nyheter)
+    
     rescue ActionController::RedirectBackError
       redirect_to root_path
   end
 
   def authenticate_editor_events!
-      flash[:error] = t('the_role.access_denied')
+    flash[:error] = t('the_role.access_denied')
     redirect_to(:back) unless current_user.admin? || current_user.has_role?(:event,:edit)
-    rescue ActionController::RedirectBackError
-      redirect_to root_path
-  end
-  
-    def authenticate_editor_poster!
-      flash[:error] = t('the_role.access_denied')
-    redirect_to(:back) unless current_user.has_role?(:poster,:edit)
-    rescue ActionController::RedirectBackError
-      redirect_to root_path
-  end
-  
-  
-  
-  
-  private
-  
-  def set_locale
-    locale = 'sv'
-    langs  = %w{ sv en ru es pl zh_CN }
-
-    if params[:locale]
-      lang = params[:locale]
-      if langs.include? lang
-        locale           = lang
-        cookies[:locale] = lang
-      end
-    else
-      if cookies[:locale]
-        lang   = cookies[:locale]
-        locale = lang if langs.include? lang
-      end
-    end
-
-    I18n.locale = locale
-    redirect_to(:back) if params[:locale]
-  end
-  
-    protected
-        before_filter :session_sanitized_params, if: :devise_controller?
-      def session_sanitized_params
-      devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:username, :password, :remember_me)}
-    end
     
-    before_filter :update_sanitized_params, if: :devise_controller?    
-    def update_sanitized_params
-      devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation)}
-    end
+    rescue ActionController::RedirectBackError
+      redirect_to root_path
   end
+  
+  def authenticate_editor_poster!
+    flash[:error] = t('the_role.access_denied')
+    redirect_to(:back) unless current_user.has_role?(:poster,:edit)
+    
+    rescue ActionController::RedirectBackError
+      redirect_to root_path
+  end
+  
+  protected
+    def configure_permitted_devise_parameters
+      devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:username, :password, :remember_me)}
+      devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:password, :password_confirmation, :current_password) }
+    end
+
+    def set_locale
+      locale = 'sv'
+      langs  = %w{ sv en }
+
+      if params[:locale]
+        lang = params[:locale]
+        if langs.include? lang
+          locale           = lang
+          cookies[:locale] = lang
+        end
+      else
+        if cookies[:locale]
+          lang   = cookies[:locale]
+          locale = lang if langs.include? lang
+        end
+      end
+
+      I18n.locale = locale
+      redirect_to(:back) if params[:locale]
+    end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,32 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable   
+require 'net/http'
+
+class User < ActiveRecord::Base 
   include TheRole::User
+  
+  after_create :create_profile_for_user
+
+  has_one :profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_one :profile
-
-  # Setup accessible (or protected) attributes for your model
-  # attr_accessible :email, :password, :password_confirmation
-  # User accessible fields
-  # attr_accessible :name, :company, :address
-
-  # When we uncomment this string - test should give fail
-  # just for example, do not uncomment it
-  # attr_accessible :some_protected_field
+  validate :is_f_member
 
 
-  
-  after_create :create_profile_for_user
-    private 
-    def create_profile_for_user
-      Profile.create(user: self)
-    end
+  def create_profile_for_user
+    Profile.create(user: self)
+  end
 
+  def is_f_member
+    errors.add :f_member, "är inte medlem i F-sektionen" unless @f_member || self.persisted?
+  end
+
+  def check_f_membership(civic)
+    url = URI.parse("http://medcheck.tlth.se/?ssid=#{civic.gsub(/[^0-9]/i, "")}")
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+
+    @f_member = res.body.include? "<img src=\"http://www.tlth.se/img/guilds/F.gif\"/>"
+  end
 end
