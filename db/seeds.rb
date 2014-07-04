dc5d3d15034e13bfad37a7538aedf724ac146c12
David Wessman <Davidwessman@live.se>
2014-07-05 01:47:15 +0200

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+
+
 
 List.find_by_category_and_name('utskott','Fös')     || List.create!(:name => 'Fös', :category => 'utskott')
 List.find_by_category_and_name('kontakt','Fös')     || List.create!(:name => 'Fös', :category => 'kontakt', :string1 => 'foset@fsektionen.se')
