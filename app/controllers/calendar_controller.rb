a90305ed9ac53570458abf3967fb76e270e8cc73
David Wessman <Davidwessman@live.se>
2014-07-07 00:46:09 +0200

    småfix

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,9 +2,8 @@
 class CalendarController < ApplicationController
     include TheRole::Controller
     
-    before_filter :login_required, only: [:show,:index]
-    before_filter :authenticate_user!, only: [:show,:index]
-
+    before_filter :login_required
+    before_filter :authenticate_user!
     
       def index
       end  
