fb693d4aed8f3bdd46961fffe25b8c399cc8ffce
David Wessman <Davidwessman@live.se>
2014-07-07 00:39:14 +0200

    kalender access

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,8 +2,8 @@
 class CalendarController < ApplicationController
     include TheRole::Controller
     
-    before_filter :login_required, only: [:show]
-    before_filter :authenticate_user!, only: [:show]
+    before_filter :login_required, only: [:show,:index]
+    before_filter :authenticate_user!, only: [:show,:index]
 
     
       def index
