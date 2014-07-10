86b1777c62b24113ae3a9dd5bc417ea37fa6ed7e
David Wessman <Davidwessman@live.se>
2014-07-11 01:11:21 +0200

    fix

--- app/assets/javascripts/galleria/galleria_setup.js
+++ app/assets/javascripts/galleria/galleria_setup.js
@@ -1,7 +1,6 @@
 Galleria.loadTheme('/assets/galleria/galleria.classicmod.js');
     Galleria.configure({
-        debug: false,
-        imageCrop: true,
+        debug: false,        
         dummy: '/assets/fallback/large_default.png',
         transition: 'fade'        
     });
