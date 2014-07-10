483b4ee88219bea190a73ffc035fec0f2e892335
David Wessman <Davidwessman@live.se>
2014-07-11 00:14:51 +0200

    fix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -8,6 +8,6 @@ class ContactMailer < ActionMailer::Base
      @email = email
      @msg = msg
      @kontakt = kontakt    
-      mail from: @name + ' <'+@email+'>', to: @kontakt.name     
+      mail from: @name + ' <'+@email+'>', to: @kontakt.string1     
   end
 end
