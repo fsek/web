263ff178fffb2f22a3548733550fff709340e5ad
David Wessman <Davidwessman@live.se>
2014-09-19 15:58:52 +0200

    Dokument och PDF fix

--- db/migrate/20410822171381_change_document_owner_to_profile_id.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDocumentOwnerToProfileId < ActiveRecord::Migration
-  def change
-    remove_column :documents,:owner,:integer
-    add_column :documents,:profile_id,:integer
-  end
-end
