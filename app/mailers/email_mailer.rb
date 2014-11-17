fa1f560f574f2659c4453d5d6ed38de712a0cd1f
David Wessman <Davidwessman@live.se>
2014-11-17 23:14:50 +0100

    länk i email-controller

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -7,7 +7,7 @@ class EmailMailer < ActionMailer::Base
     @email = email
     if(@email.copy)
       Rails.logger.info @account.email
-      mail(from: @account.title + "<"+@account.email+">",to: @email.receiver,subject: @email.subject,bcc: @account.email, cc: @account.email)
+      mail(from: @account.title + "<"+@account.email+">",to: @email.receiver,subject: @email.subject,bcc: @account.email, bcc: @account.email)
     else
       mail(from: @account.title + "<"+@account.email+">",to: @email.receiver,subject: @email.subject)
     end
