240accdfd08be2ccfc6737bccbebe93278d75146
David Wessman <Davidwessman@live.se>
2015-03-05 15:49:55 +0100

    Wrong mailer in nomination

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -8,6 +8,6 @@ class Nomination < ActiveRecord::Base
   after_create :send_email
 
   def send_email
-    ElectionMailer.nomination_email(self).deliver
+    ElectionMailer.nominate_email(self).deliver
   end
 end
