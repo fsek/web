32acf1baf22451b333935506503b91cb32c86346
Erik Henriksson <erik.henriksson@me.com>
2015-03-11 22:34:32 +0100

    Fix travis failure

--- spec/controllers/githook_controller_spec.rb
+++ spec/controllers/githook_controller_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe GithookController, type: :controller do
   describe "githook", :type => :request do
-    it "returns bad request on invalid request" do
-      params = {}
-      post "githook#index", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
-      expect(response).to have_http_status(:bad_request)
+    it "requires authentication" do
+      get "githook/master"
+      expect(response).to have_http_status(:found)
     end
   end
 end
