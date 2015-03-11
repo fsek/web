576d6c44b297e1f423e28de8f3f2303c0fd992f6
Erik Henriksson <erik.henriksson@me.com>
2015-03-11 18:18:33 +0100

    Hound fixes

--- app/controllers/githook_controller.rb
+++ app/controllers/githook_controller.rb
@@ -48,7 +48,10 @@ class GithookController < ApplicationController
   end
 
   def verify_signature(payload_body)
-      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
-      return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(
+      OpenSSL::Digest.new('sha1'),
+      ENV['SECRET_TOKEN'], payload_body)
+    return halt 500, "Signatures didn't match!" unless
+      Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
   end
 end
