93acc036ca9a261de60b70ef9ed845db05f5a7c0
Erik Henriksson <erik.henriksson@me.com>
2015-03-12 14:51:17 +0100

    Remove database password and create symlink on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,3 +14,13 @@ end
 
 before "deploy:setup", 'paperclip:create_storage'
 after "deploy:update_code", "paperclip:link_storage"
+
+# Symlink the database
+after 'deploy:update_code', 'deploy:symlink_db'
+
+namespace :deploy do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+end
