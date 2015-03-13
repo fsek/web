bc7acffacab49e687b61a90f5b2e231aa90b7f5c
Linus Hammarlund <linus.hammarlund@gmail.com>
2015-03-13 21:40:05 +0100

    Removed autowrapper on error to make file upload work

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
-
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Fsek::Application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
