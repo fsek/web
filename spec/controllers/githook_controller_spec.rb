a5c2bef89cc7fd0ebefe1ff7ad3ef3cf03f79106
Erik Henriksson <erikhenrikssn@gmail.com>
2015-03-16 22:22:47 +0100

    Fix rails 4.2 githook controller issues

--- spec/controllers/githook_controller_spec.rb
+++ spec/controllers/githook_controller_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe GithookController, type: :controller do
   describe "githook", :type => :request do
     it "requires authentication" do
-      get "githook/master"
+      get "/githook/master"
       expect(response).to have_http_status(:found)
     end
     it 'requires sha hash' do
-      post 'githook', '', { HTTP_X_HUB_SIGNATURE: '' }
+      post '/githook', '', { HTTP_X_HUB_SIGNATURE: '' }
       expect(response).to have_http_status(500)
     end
     it 'accepts github token' do
       json_payload = '{"ref": "refs/heads/foo"}'
-      post 'githook', json_payload, { format: 'json', HTTP_X_HUB_SIGNATURE: 'sha1=87db15ae6db2fa97763ebad0926c8389fdbbc325' }
+      post '/githook', json_payload, { format: 'json', HTTP_X_HUB_SIGNATURE: 'sha1=87db15ae6db2fa97763ebad0926c8389fdbbc325' }
       expect(response).to have_http_status(200)
     end
   end
