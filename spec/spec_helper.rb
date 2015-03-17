df169c16c2346ce0e8e93a3c2b52d248a55761eb
Erik Henriksson <erikhenrikssn@gmail.com>
2015-03-17 17:10:26 +0100

    Hound fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,8 +19,8 @@ CodeClimate::TestReporter.start
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require File.expand_path("../../config/environment", __FILE__)
-require "rails/all"
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/all'
 require 'rspec/rails'
 # Add this to load Capybara integration:
 require 'capybara/rspec'
@@ -35,9 +35,9 @@ RSpec.configure do |config|
     # and `failure_message` of custom matchers include text for helper methods
     # defined using `chain`, e.g.:
     #     be_bigger_than(2).and_smaller_than(4).description
-    #     # => "be bigger than 2 and smaller than 4"
+    #     # => 'be bigger than 2 and smaller than 4'
     # ...rather than:
-    #     # => "be bigger than 2"
+    #     # => 'be bigger than 2'
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
