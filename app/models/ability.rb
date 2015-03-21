ccf2593ba0bb08c0012c500dc189a4bedf523a41
Erik Henriksson <erikhenrikssn@gmail.com>
2015-03-21 16:03:20 +0100

    Cancan dynamic permission model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,5 +11,16 @@ class Ability
     if user.is? :admin
       can :manage, :all
     end
+
+    # Add abilities gained from posts
+    user.posts.each do |post|
+      post.permissions.each do |permission|
+        if permission.subject_class == "all"
+          can permission.action.to_sym, permission.subject_class.to_sym
+        else
+          can permission.action.to_sym, permission.subject_class.constantize
+        end
+      end
+    end
   end
 end
