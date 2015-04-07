215b64318801f6f270448cb63c39e76bb7ea2ea8
Erik Henriksson <erikhenrikssn@gmail.com>
2015-04-07 11:27:31 +0200

    Add test to the promote_admin rake task

---
+++ spec/lib/tasks/promote_admin_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'promote_admin' do
+  include_context 'rake'
+
+  before do
+    create(:user)
+  end
+
+  it 'promotes the first user to admin' do
+    subject.invoke
+    Ability.new(User.first).can?(:manage, :all).should eq(true)
+  end
+end
