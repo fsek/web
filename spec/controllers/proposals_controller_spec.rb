d112c31b37ad7d8b3d8e6d02faf2ef23a54118ae
Johan Förberg <johan@forberg.se>
2015-05-07 22:03:00 +0200

    Test should of course render_views.

--- spec/controllers/proposals_controller_spec.rb
+++ spec/controllers/proposals_controller_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe ProposalsController, :type => :controller do
   describe '#generate' do
+    render_views
+
     it 'doesn\'t die horribly when you try to use it' do
       lambda do
         post :generate, :format => :pdf, :proposal => 
-          { :title => 'my proposal' }
+          { :title => 'my proposal', :points => [] }
       end.should_not raise_error
       response.should be_success
     end
