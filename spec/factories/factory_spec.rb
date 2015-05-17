71674aa1ace78102816b59add1741201cd91ee95
davidwessman <Davidwessman@live.se>
2015-05-17 15:14:48 +0200

    Hound fixes

--- spec/factories/factory_spec.rb
+++ spec/factories/factory_spec.rb
@@ -1,8 +1,7 @@
 FactoryGirl.factories.map(&:name).each do |factory_name|
   describe "#{factory_name} factory" do
-
     # Test each factory
-    it "is valid" do
+    it 'is valid' do
       factory = FactoryGirl.build(factory_name)
       if factory.respond_to?(:valid?)
         factory.should be_valid, lambda { factory.errors.full_messages.join("\n") }
