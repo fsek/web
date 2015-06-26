4cbbb637e55e58aeddcde9228924f7eaee04adc9
davidwessman <Davidwessman@live.se>
2015-06-26 22:13:36 +0200

    Hound fixes

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -8,112 +8,112 @@ RSpec.describe GroupsController, type: :controller do
 
   allow_user_to :manage, Group
 
-  describe "GET #index" do
-    it "assigns" do
+  describe 'GET #index' do
+    it 'assigns' do
       group
       get :index
       assigns(:groups).should include(group)
     end
   end
 
-  describe "GET #show" do
-    it "assigns the requested group as @group" do
+  describe 'GET #show' do
+    it 'assigns the requested group as @group' do
       get :show, id: group.to_param
       assigns(:group).should eq(group)
     end
   end
 
-  describe "GET #new" do
-    it "assigns a new group as @group" do
+  describe 'GET #new' do
+    it 'assigns a new group as @group' do
       get :new
       assigns(:group).should be_a_new(Group)
     end
   end
 
-  describe "GET #edit" do
-    it "assigns the requested group as @group" do
+  describe 'GET #edit' do
+    it 'assigns the requested group as @group' do
       get :edit, id: group.to_param
       assigns(:group).should eq(group)
     end
   end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Group" do
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new Group' do
         lambda do
           post :create, group: valid_attr
         end.should change(Group, :count).by(1)
       end
 
-      it "assigns a newly created group as @group" do
+      it 'assigns a newly created group as @group' do
         post :create, group: valid_attr
         assigns(:group).should be_a(Group)
         assigns(:group).should be_persisted
       end
 
-      it "redirects to the created group" do
+      it 'redirects to the created group' do
         post :create, group: valid_attr
         response.should redirect_to(Group.last)
       end
     end
 
-    context "with invalid params" do
-      it "assigns a newly created but unsaved group as @group" do
+    context 'with invalid params' do
+      it 'assigns a newly created but unsaved group as @group' do
         post :create, group: invalid_attr
         assigns(:group).should be_a_new(Group)
       end
 
-      it "re-renders the 'new' template" do
+      it 're-renders the 'new' template' do
         post :create, group: invalid_attr
-        response.should render_template("new")
+        response.should render_template('new')
       end
     end
   end
 
-  describe "PATCH #update" do
-    context "with valid params" do
-      it "updates the requested group" do
+  describe 'PATCH #update' do
+    context 'with valid params' do
+      it 'updates the requested group' do
         patch :update, id: group.to_param, group: new_attr
         group.reload
         group.title.should eq(new_attr[:title])
       end
 
-      it "assigns the requested group as @group" do
+      it 'assigns the requested group as @group' do
         patch :update, id: group.to_param, group: valid_attr
         assigns(:group).should eq(group)
       end
 
-      it "redirects to the group" do
+      it 'redirects to the group' do
         patch :update, id: group.to_param, group: valid_attr
         response.should redirect_to(group)
       end
     end
 
-    context "with invalid params" do
-      it "assigns the group as @group" do
+    context 'with invalid params' do
+      it 'assigns the group as @group' do
         patch :update, id: group.to_param, group: invalid_attr
         assigns(:group).should eq(group)
       end
 
-      it "re-renders the 'edit' template" do
+      it 're-renders the "edit" template' do
         patch :update, id: group.to_param, group: invalid_attr
-        response.should render_template("edit")
+        response.should render_template('edit')
       end
     end
   end
 
-  describe "DELETE #destroy" do
+  describe 'DELETE #destroy' do
     before(:each) do
       group
     end
 
-    it "destroys the requested group" do
+    it 'destroys the requested group' do
       lambda do
         delete :destroy, id: group.to_param
       end.should change(Group, :count).by(-1)
     end
 
-    it "redirects to the groups list" do
+    it 'redirects to the groups list' do
       delete :destroy, id: group.to_param
       response.should redirect_to(groups_path)
     end
