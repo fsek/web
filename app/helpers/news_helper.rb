4cc53180aa210cb1a4da69d110681e78656de586
davidwessman <david.wesmn@gmail.com>
2016-01-10 22:33:55 +0100

    Adding controller spec for contacts

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -3,7 +3,7 @@ module NewsHelper
     if news.present? && news.url.present?
       content_tag(:span, class: :link) do
         link_to(news.url, target: :blank) do
-          fa_icon('link') + " " + news.url
+          fa_icon('link') + ' ' + news.url
         end
       end
     end
@@ -13,7 +13,7 @@ module NewsHelper
     if news.present? && news.user.present?
       content_tag(:span, class: :link) do
         link_to(news.user, target: :blank) do
-          fa_icon('user') + " " + news.user
+          fa_icon('user') + ' ' + news.user
         end
       end
     end
