6a08981e440cd2c93848bb38cf3f96e9c1455e2c
davidwessman <david.wesmn@gmail.com>
2016-02-23 10:49:54 +0100

    Election mailer localizing
    Localizing all emails
    Using markdown to render texts
    Adding some helper methods and specs for it

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,12 +1,28 @@
 module MarkdownHelper
+  require 'redcarpet/render_strip'
   def markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-                                       no_intra_emphasis: true,
-                                       fenced_code_blocks: true,
-                                       autolink: true,
-                                       tables: true,
-                                       underline: true,
-                                       highlight: true)
-    ActionView::Base.new.sanitize(markdown.render(text).html_safe)
+    if text.present?
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                         no_intra_emphasis: true,
+                                         fenced_code_blocks: true,
+                                         autolink: true,
+                                         tables: true,
+                                         underline: true,
+                                         highlight: true)
+      ActionView::Base.new.sanitize(markdown.render(text).html_safe)
+    end
+  end
+
+  def markdown_plain(text)
+    if text.present?
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::StripDown,
+                                         no_intra_emphasis: true,
+                                         fenced_code_blocks: true,
+                                         autolink: true,
+                                         tables: true,
+                                         underline: true,
+                                         highlight: true)
+      ActionView::Base.new.sanitize(markdown.render(text).html_safe)
+    end
   end
 end
