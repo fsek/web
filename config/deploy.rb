c93aa7bd76d0c33e32e43b7c067dbfd76ec0f19a
davidwessman <david.wesmn@gmail.com>
2016-04-25 14:54:08 +0200

    Rollbar error reporting
    Integrating Rollbar for error reporting
    Implementing with Capistrano to reset on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,10 +13,15 @@ set :use_sudo, false
 set :bundle_binstubs, nil
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml .rbenv-vars}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp vendor/bundle public/system public/uploads storage public/sitemaps}
+
+# Rollbar
+set :rollbar_token, ENV['ROLLBAR_TOKEN']
+set :rollbar_env, Proc.new { fetch :stage }
+set :rollbar_role, Proc.new { :app }
 
 after 'deploy:publishing', 'passenger:restart'
 after 'deploy:publishing', 'deploy:sitemap:refresh'
