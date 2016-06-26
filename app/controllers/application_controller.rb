621a9d56020e4158396f018f61a8d506263badd6
davidwessman <david.wesmn@gmail.com>
2016-06-26 11:32:44 +0200

    Redirects to current path after sign in
    Adds store_location_for to application controller
    Adds spec to test login and redirect
    Adds autofocus to email-field on sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_devise_parameters, if: :devise_controller?
+  before_action :store_current_location, unless: :devise_controller?
   before_action :set_locale
 
   helper_method :alert_update, :alert_create, :alert_destroy,
@@ -13,7 +14,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |ex|
     if current_user.nil?
-      redirect_to :new_user_session, alert: ex.message
+      redirect_to(new_user_session_path, alert: ex.message)
     else
       redirect_to :root, alert: ex.message
     end
@@ -54,6 +55,11 @@ class ApplicationController < ActionController::Base
 
   def referring_action
     Rails.application.routes.recognize_path(request.referer)[:action]
+  end
+
+  # Makes redirect after sign_in work
+  def store_current_location
+    store_location_for(:user, request.url)
   end
 
   def recache_menu
---
+++ spec/features/user/log_in_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+RSpec.feature 'User sign in' do
+  let(:user) { create(:user) }
+
+  scenario 'sign in correct credentials' do
+    page.visit(new_user_session_path)
+    page.fill_in 'user_email', with: user.email
+    page.fill_in 'user_password', with: '12345678'
+    page.click_button I18n.t('devise.sign_in')
+    page.should have_css('div.alert.alert-info')
+    find('div.alert.alert-info').text.should include(I18n.t('devise.sessions.signed_in'))
+  end
+
+  scenario 'sign in incorrect password' do
+    page.visit(new_user_session_path)
+    page.fill_in 'user_email', with: user.email
+    page.fill_in 'user_password', with: 'wrong'
+    page.click_button I18n.t('devise.sign_in')
+    page.should have_css('div.alert.alert-danger')
+    find('div.alert.alert-danger').text.should include(I18n.t('devise.failure.invalid'))
+  end
+
+  scenario 'sign in non existing email' do
+    page.visit(new_user_session_path)
+    page.fill_in 'user_email', with: 'not-existing@email.com'
+    page.fill_in 'user_password', with: 'wrong'
+    page.click_button I18n.t('devise.sign_in')
+    page.should have_css('div.alert.alert-danger')
+    find('div.alert.alert-danger').text.should \
+      include(I18n.t('devise.failure.user.not_found_in_database'))
+  end
+
+  scenario 'redirected back to the right page' do
+    page.visit(new_rent_path)
+    find('div.alert.alert-danger').text.should include(I18n.t('unauthorized.new.rent'))
+    page.fill_in 'user_email', with: user.email
+    page.fill_in 'user_password', with: '12345678'
+    page.click_button I18n.t('devise.sign_in')
+    page.should have_css('div.alert.alert-info')
+    find('div.alert.alert-info').text.should include(I18n.t('devise.sessions.signed_in'))
+    page.current_path.should eq(new_rent_path)
+  end
+end
