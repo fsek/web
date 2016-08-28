071a508bf0502ad7c4cafc4895313769d3269392
davidwessman <david.wesmn@gmail.com>
2016-08-28 23:42:12 +0200

    User: add print program method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,11 @@ module UsersHelper
   def display_phone?(user:, current_user:)
     user.display_phone? && user.phone.present? && user.groups.merge(current_user.groups).any?
   end
+
+  def user_program_collection
+    [[I18n.t('model.user.program.physics'), User::PHYSICS],
+     [I18n.t('model.user.program.math'), User::MATH],
+     [I18n.t('model.user.program.nano'), User::NANO],
+     [I18n.t('model.user.program.other'), User::OTHER]]
+  end
 end
