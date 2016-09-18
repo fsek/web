3087485372ed048ab31c4daae7df9d1496a2a530
davidwessman <david.wesmn@gmail.com>
2016-09-18 11:29:50 +0200

    Album: show all albums to new students

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -5,15 +5,8 @@ class GalleryController < ApplicationController
     @year = params[:year] || Time.zone.now.year
     @all_years = Album.unique_years - [@year.to_i]
 
-    @albums = album(Album.include_for_gallery.gallery(Time.zone.local(@year, 3)))
+    @albums = Album.include_for_gallery.gallery(Time.zone.local(@year, 3))
 
     @policy = Document.find_by(slug: :gallery_policy)
-  end
-
-  private
-
-  # Only temporary from beginning of August
-  def album(albums)
-    (current_user.nil? || current_user.summerchild?) ? albums.summer : albums
   end
 end
