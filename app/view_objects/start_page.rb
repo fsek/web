4a22c97c76e0d7273913f22e20940f37b3d542d9
davidwessman <david.wesmn@gmail.com>
2016-09-19 11:36:20 +0200

    Introduction: add countdown to start page

--- app/view_objects/start_page.rb
+++ app/view_objects/start_page.rb
@@ -1,10 +1,12 @@
 class StartPage
   attr_accessor :news, :events, :notices
+  attr_reader :countdown
 
   def initialize(member: false)
     @news = News.include_for_feed.by_date.limit(5) || []
     @events = Event.by_locale(locale: I18n.locale).stream || []
     @notices = get_notices(member)
+    @countdown = Time.zone.now.change(month: 9, day: 22, hour: 16, minute: 0, second: 0)
   end
 
   private
