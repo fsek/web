e1c7d6889ac9dc00ca34791576073c9f65b8792a
David Wessman <david.wesmn@gmail.com>
2016-12-26 21:33:50 +0100

    Menu: change rendering
    - adds helper methods to handle options
    - adds separate partial for header

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,7 +1,13 @@
+# Helps creating menu links
 module MenuHelper
-
   def menu_dropdown_render(menus, cls: '')
-    content_tag(:ul, render(partial: 'menus/menu', collection: menus), class: cls)
+    content_tag(:ul, class: cls) do
+      content = []
+      menus.each do |m|
+        content << render(m.to_partial_path, menu: m)
+      end
+      safe_join(content)
+    end
   end
 
   def menu_locale_link(link, locale)
@@ -12,5 +18,28 @@ module MenuHelper
         link
       end
     end
+  end
+
+  def menu_link(menu, locale)
+    link_to(menu_content(menu), menu_locale_link(menu.link, locale),
+            menu_options(menu.blank_p, menu.turbolinks))
+  end
+
+  def menu_content(menu)
+    content = [menu.to_s]
+    content << fa_icon('external-link') if menu.blank_p
+    safe_join(content)
+  end
+
+  def menu_options(blank, turbolinks)
+    options = {}
+    options[:target] = :blank if blank
+    options['data-turbolinks'] = turbolinks.to_s
+    options
+  end
+
+  def menu_class(blank)
+    return "menu blank" if blank
+    "menu"
   end
 end
