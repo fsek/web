fa860763a574fff2fddb8700a2959c17dbebb767
Jakob Navrozidis <jakob@navrozidis.com>
2017-02-23 14:57:53 +0100

    Translations for Councils and Posts
    Adds translations for `Council` and `Post`

--- app/models/council.rb
+++ app/models/council.rb
@@ -13,7 +13,7 @@ class Council < ActiveRecord::Base
   has_many :cafe_workers, through: :cafe_worker_councils
   has_many :cafe_shifts, through: :cafe_workers
 
-  translates(:title)
+  translates(:title, fallbacks_for_empty_translations: true)
   globalize_accessors(locales: [:en, :sv], attributes: [:title])
 
   # Validation
