61f15385374827a23ef22b2a4db2c5e0081d59f1
navro <jakob@navrozidis.com>
2017-03-25 22:45:26 +0100

    Song update

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,6 +6,15 @@ class SongsController < ApplicationController
   end
 
   def show
-    @songs = initialize_grid(Song.all)
+  end
+
+  def search
+    @songs = Song.title_search(search_params[:title])
+  end
+
+  private
+
+  def search_params
+    params.require(:song).permit(:title)
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,10 @@ class Song < ActiveRecord::Base
   def to_s
     title
   end
+
+  def self.title_search(title)
+    if title.present?
+      Song.where('title LIKE ?', "%#{title}%").limit(20)
+    end
+  end
 end
