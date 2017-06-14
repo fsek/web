18c345100d919d52f5839f0ae07a53890f6a8880
Jakob Navrozidis <jakob@navrozidis.com>
2017-06-14 14:10:24 +0200

    fixup! API: Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -789,12 +789,12 @@ ActiveRecord::Schema.define(version: 20170823132130) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                                default: "",      null: false
+    t.string   "encrypted_password",                   default: "",      null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",                        default: 0,       null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -818,12 +818,12 @@ ActiveRecord::Schema.define(version: 20170823132130) do
     t.datetime "member_at"
     t.string   "food_custom"
     t.string   "student_id"
-    t.boolean  "display_phone",          default: false, null: false
+    t.boolean  "display_phone",                        default: false,   null: false
     t.string   "food_preferences"
-    t.integer  "notifications_count",    default: 0,     null: false
-    t.string   "provider",               default: "email", null: false
-    t.string   "uid",                    default: "",      null: false
-    t.text     "tokens"
+    t.string   "provider",                             default: "email", null: false
+    t.string   "uid",                                  default: "",      null: false
+    t.text     "tokens",                 limit: 65535
+    t.integer  "notifications_count",                  default: 0,       null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
