275b9b2b86dd9c54e3385685f744bee0ca289e1b
olofenglund96 <olofenglund96@gmail.com>
2018-01-23 22:40:11 +0100

    Adding bootstrap color vars

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -1,16 +1,18 @@
 $(document).on('turbolinks:load', function() {
   // Ensures code only runs on pages containing event errors
-  var ee = $(".event-errors")
-  if(ee != null) {
-    var tabs = $(".new_event, .edit_event").find(".tab-pane");
+  var ee = $('.event-errors')
+  if(ee !== null) {
+    var tabs = $('.new_event, .edit_event').find('.tab-pane');
 
     tabs.each(function() {
       // Finds the sidemenu-link corresponding to the 
       // tab containing the error and add error class
-      if($(this).find(".form-group").hasClass("has-error")) {
-        ee.find('a[href="#' + $(this).attr('id')+'"]').parent(".list-group-item").addClass("event-error")
+      if($(this).find('.form-group').hasClass('has-error')) {
+        ee.find('a[href="#' + $(this)
+        .attr('id')+'"]')
+        .parent('.list-group-item')
+        .addClass('event-error')
       }
     })
   }
 })
-
