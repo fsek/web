dee8c530f13d4bcad23067055c22c6422ab84375
olofenglund96 <olofenglund96@gmail.com>
2018-01-30 15:42:10 +0100

    Various small changes

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -2,15 +2,15 @@ class Api::SongsController < Api::BaseController
   load_permissions_and_authorize_resource
 
   def index
-    @songs = Song.all
+    @songs = Song.order(title: :asc)
     render json: @songs,
-    each_serializer: Api::SongSerializer::Multiple # Returns id and title
+    each_serializer: Api::SongSerializer::Index # Returns id and title
   end
 
   def show
     @song = Song.find(params[:id])
     Song.increment_counter(:visits, @song)
     render json: @song,
-    serializer: Api::SongSerializer::Single # Returns all relevant fields
+    serializer: Api::SongSerializer::Show # Returns all relevant fields
   end
 end
