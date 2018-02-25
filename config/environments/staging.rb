32f0a4d4d35d82baff95c794ce45e5e479784230
Jakob Navrozidis <jakob@navrozidis.com>
2018-02-25 18:25:03 +0100

    Update uglifier gems
    Updates uglifier + dependencies.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -23,9 +23,8 @@ Rails.application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.public_file_server.enabled = false
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
+  # Compress JavaScripts
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
