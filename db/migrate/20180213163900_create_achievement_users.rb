8cf2a6fe8a5c7820ec54a3dd7b17ebf212de41ab
David <brante_krantz@hotmail.com>
2018-03-25 18:52:50 +0200

    Minor fixes

--- db/migrate/20180213163900_create_achievement_users.rb
+++ db/migrate/20180213163900_create_achievement_users.rb
@@ -1,8 +1,9 @@
 class CreateAchievementUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :achievement_users do |t|
-      t.references :achievement, foreign_key: true, index: true, unique: true
-      t.references :user, foreign_key: true, index: true, unique: true
+      t.references :achievement, foreign_key: true, index: true
+      t.references :user, foreign_key: true, index: true
+      t.index [:achievement_id, :user_id], unique: true
       t.timestamps null: false
     end
   end
