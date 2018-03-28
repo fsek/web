37bfdc36e558edb93bff057d0a8b2a6016a2b0d2
David <brante_krantz@hotmail.com>
2018-03-28 13:07:00 +0200

    Fixes #708

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,7 +7,7 @@ class Song < ApplicationRecord
 
   def self.title_search(title)
     if title.present?
-      Song.where('title LIKE ?', "%#{title}%").limit(10)
+      Song.where('title ILIKE ?', "%#{title}%").limit(10)
     end
   end
 
