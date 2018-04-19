c25e661b0ca772918a400e28a8a029c691eb3e51
David <brante_krantz@hotmail.com>
2018-04-19 22:09:05 +0200

    Added API for picture gallery

---
+++ app/controllers/api/albums_controller.rb
@@ -0,0 +1,10 @@
+class Api::AlbumsController < Api::BaseController
+  load_permissions_then_authorize_resource
+
+  def show
+    @album = Album.include_for_gallery.includes(:photographers).find(params[:id])
+
+    render json: @album,
+    serializer: Api::AlbumSerializer::Show
+  end
+end
