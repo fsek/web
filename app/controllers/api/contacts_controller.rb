12aa65266fce0d4ba4c0b1996c262cafa2dbda9e
Olof Englund <olofenglund96@gmail.com>
2018-04-24 14:41:27 +0200

    Contacts are no longer exposed to the public api

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,18 +1,26 @@
 class Api::ContactsController < Api::BaseController
-  load_permissions_and_authorize_resource
+  # Special auth due to api using this controller.
+  # We do not want to expose emails and such.
+  before_action :load_permissions
 
   def index
+    authorize!(:index, :contact)
+
     # We need ruby sorting here due to dynamic naming
-    @contacts = @contacts.includes(:post).sort_by(&:to_s)
+    @contacts = Contact.includes(:post).sort_by(&:to_s)
     render json: @contacts, each_serializer: Api::ContactsSerializer::Index
   end
 
   def show
+    authorize!(:show, :contact)
+
     @contact = Contact.find(params[:id])
     render json: @contact, serializer: Api::ContactsSerializer::Show
   end
 
   def mail
+    authorize!(:mail, :contact)
+
     @contact = Contact.find(params[:id])
     @contact.message ||= ContactMessage.new(message_params)
     if @contact.send_email
