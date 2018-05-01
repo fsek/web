c484bd5fc1395ad23f28dbc8c2a77f3cbabe0143
Jakob Navrozidis <jakob@navrozidis.com>
2018-05-01 22:35:03 +0200

    Add sidekiq-cron and Fredmansky cronjob

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,5 @@
 redis = { url: "redis://localhost:6379/#{ENV['SIDEKIQ_DB_NUMBER'] || 0}" }
+schedule_file = "config/schedule.yml"
 
 if Rails.env == "test"
   redis = { url: "redis://localhost:6379/0" }
@@ -6,6 +7,10 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = redis
+
+  if File.exist?(schedule_file) && Sidekiq.server?
+    Sidekiq::Cron::Job.load_from_hash!(YAML.load_file(schedule_file))
+  end
 end
 
 Sidekiq.configure_client do |config|
