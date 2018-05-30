050d0d7d21ad880b3cc789f4e88a78bc59ddf7ef
David <brante_krantz@hotmail.com>
2018-05-30 21:34:50 +0200

    Event signup closing notification changes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@ class Api::UsersController < Api::BaseController
 
   def update
     @user = current_user
-    
+
     if @user.update(user_params)
       render json: {}, status: :ok
     else
@@ -17,6 +17,6 @@ class Api::UsersController < Api::BaseController
     params.require(:user).permit(:firstname, :lastname, :program, :start_year,
                                  :avatar, :student_id, :phone, :display_phone,
                                  :remove_avatar, :food_custom, :notify_messages,
-                                 :notify_event_users, food_preferences: [])
+                                 :notify_event_users, :notify_event_closing, food_preferences: [])
   end
 end
