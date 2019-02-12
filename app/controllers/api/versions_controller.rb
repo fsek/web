68a26ba318df18a0c4e354d0bb6235cb9b45e8f7
Jakob Navrozidis <jakob@navrozidis.com>
2019-02-12 16:08:10 +0100

    Fix current version for API versioning

--- app/controllers/api/versions_controller.rb
+++ app/controllers/api/versions_controller.rb
@@ -2,7 +2,7 @@ class Api::VersionsController < Api::BaseController
   # Don't forget to change this when adding app-breaking changes
   def index
     render json: {
-      current_version: Versions.get(:api),
+      current_version: Versions.get(:api).to_s,
       api_version: Versions.get(:api),
       terms_version: Versions.get(:terms)
     }
