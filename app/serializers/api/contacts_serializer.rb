b6623eb068e90ef47a7fa853f39ad2cc36d8ca00
David <brante_krantz@hotmail.com>
2019-03-07 21:35:11 +0100

    Contact avatar improvement in API

--- app/serializers/api/contacts_serializer.rb
+++ app/serializers/api/contacts_serializer.rb
@@ -1,12 +1,30 @@
 class Api::ContactsSerializer < ActiveModel::Serializer
-  class Api::ContactsSerializer::Show < ActiveModel::Serializer
+  include ContactHelper
+
+  class Api::ContactsSerializer::Show < Api::ContactsSerializer
     attributes(:email, :name, :text, :avatar)
     attribute(:users) { object.users.collect { |u| u.firstname + ' ' + u.lastname } }
+
+    def avatar
+      if contact_single_user(object)
+        object.users.first.avatar
+      else
+        object.avatar
+      end
+    end
   end
 
-  class Api::ContactsSerializer::Index < ActiveModel::Serializer
+  class Api::ContactsSerializer::Index < Api::ContactsSerializer
     attributes(:id, :email, :name, :text, :avatar)
     attribute(:users) { object.users.collect { |u| u.firstname + ' ' + u.lastname } }
+
+    def avatar
+      if contact_single_user(object)
+        object.users.first.avatar
+      else
+        object.avatar
+      end
+    end
 
     def text
       MarkdownHelper.markdown_api(object.text)
