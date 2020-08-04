5fff2da31183c443bae61a20b5240bc425fe818a
David <brante_krantz@hotmail.com>
2020-08-04 16:16:02 +0200

    Changes the names of the Fös

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -87,7 +87,23 @@ module PostHelper
             t('helper.post.who_currently')
           end
         end
-        content << post_users_list(post)
+        if post.title == 'Överfös'
+          names = ['Överfös Lovisa']
+          names.each do |e|
+            content << content_tag(:li) do
+              e
+            end
+          end
+        elsif post.title == 'Cofös'
+          names = ['Cofös Henrik', 'Cofös Saga', 'Cofös Matilda', 'Cofös Björn']
+          names.each do |e|
+            content << content_tag(:li) do
+              e
+            end
+          end
+        else
+          content << post_users_list(post)
+        end
         safe_join(content)
       end
     end
