6e793e8db863fe59431912351006567b41f31b46
Johanna Gustafson <gystafson@gmail.com>
2020-09-29 17:32:54 +0200

    Automatic granting of access

---
+++ app/controllers/doors_controller.rb
@@ -0,0 +1,17 @@
+class DoorsController < ApplicationController
+  def index
+    @door_grid = initialize_grid(Door)
+  end
+
+  def accesses
+    @doors = Door.by_title
+    @door = Door.includes(:posts).find(params[:id])
+    render layout: false
+  end
+
+  private
+
+  def door_params
+    params.require(:door).permit(:title, post_ids: [])
+  end
+end
