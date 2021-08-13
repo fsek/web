f2aaf4056d08cfa55aa975d67cff1e5f4404e068
Sebastian Froste <se1215fr-s@student.lu.se>
2021-08-13 17:15:34 +0200

    Uppdaterar nollningssidan

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210228161800) do
+ActiveRecord::Schema.define(version: 20201220144612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -460,13 +460,6 @@ ActiveRecord::Schema.define(version: 20210228161800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_fredmanskies_on_user_id"
-  end
-
-  create_table "fruits", id: :serial, force: :cascade do |t|
-    t.integer "user_id"
-    t.string "name", null: false
-    t.boolean "is_moldy", default: false, null: false
-    t.index ["user_id"], name: "index_fruits_on_user_id"
   end
 
   create_table "group_messages", id: :serial, force: :cascade do |t|
