2da1cc47d51f874c36edfb792f9474c038407dcc
Lukas <lukas.gustavsson@hotmail.com>
2022-11-12 16:39:35 +0100

    removing test that is no longer applicable...

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -9,20 +9,6 @@ RSpec.describe Event, type: :model do
     build_stubbed(:event, :with_signup).should be_valid
   end
 
-  describe 'by locale' do
-    it 'returns event depending on locale' do
-      create(:event, title_sv: 'Ej översatt', title_en: 'Not translated')
-      event_en = create(:event, title_sv: 'Kommer att översättas', title_en: 'Kommer att översättas')
-      event_en.update!(title_en: 'Will be translated')
-
-      Event.by_locale.map(&:title).should eq(['Ej översatt',
-                                              'Kommer att översättas'])
-      Event.by_locale(locale: 'en').order(:created_at).map(&:title_en).should eq(['Not translated',
-                                                                                  'Will be translated'])
-      Event.by_locale(locale: 'nope').should be_empty
-    end
-  end
-
   # This tests makes sure that dates are formatted into ISO8601 for
   # Fullcalendars json-feed
   # Ref.: https://github.com/fsek/web/issues/99
