0def49896bb0c72cd950c289a98da4beeb549114
Oskar Watsfeldt <oskarwats@hotmail.com>
2022-12-01 00:02:49 +0100

    now with moose

--- db/migrate/20221130135000_add_nickname_to_user_edit.rb
+++ db/migrate/20221130135000_add_nickname_to_user_edit.rb
@@ -1,5 +1,5 @@
 class AddNicknameToUserEdit < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :game_nickname, :string, null: true
+    add_column :users, :moose_game_nickname, :string, null: true
   end
 end
