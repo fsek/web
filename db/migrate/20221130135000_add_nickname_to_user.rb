15d6b0ca40bcbda9c1b9e72b44890d72bcbbb54f
Oskar Watsfeldt <oskarwats@hotmail.com>
2022-11-30 23:39:38 +0100

    needed other name

--- db/migrate/20221130135000_add_nickname_to_user.rb
+++ db/migrate/20221130135000_add_nickname_to_user.rb
@@ -1,4 +1,4 @@
-class AddNicknameToUser < ActiveRecord::Migration[5.1]
+class AddNicknameToUserSoICanRevert < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :moose_game_nickname, :string, null: true
   end
