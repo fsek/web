68c93d630fb36e823a55342f09d94b09acb40ba2
Georg <50551748+georgelgeback@users.noreply.github.com>
2025-06-21 16:13:08 +0200

    Vibe coded fix

--- app/serializers/api/game_score_serializer.rb
+++ app/serializers/api/game_score_serializer.rb
@@ -1,5 +1,12 @@
 class Api::GameScoreSerializer <  ActiveModel::Serializer
   class Api::GameScoreSerializer::Index < ActiveModel::Serializer
     attributes(:id, :user, :score)
+
+    def user
+      {
+        id: object.user.id,
+        game_nickname: object.user.game_nickname
+      }
+    end
   end
 end
