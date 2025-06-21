5d0e8a8453cb398377d52d4b708135c271d8c902
HellFelix <61821031+HellFelix@users.noreply.github.com>
2025-06-21 16:59:55 +0200

    [API] (Update): Remove full user info from game_score_serializer

--- app/serializers/api/game_score_serializer.rb
+++ app/serializers/api/game_score_serializer.rb
@@ -1,12 +1,5 @@
 class Api::GameScoreSerializer <  ActiveModel::Serializer
   class Api::GameScoreSerializer::Index < ActiveModel::Serializer
-    attributes(:id, :user, :score)
-
-    def user
-      {
-        id: object.user.id,
-        game_nickname: object.user.game_nickname
-      }
-    end
+    attributes(:id, :name, :score)
   end
 end
